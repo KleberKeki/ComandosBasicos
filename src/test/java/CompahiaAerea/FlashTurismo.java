861cdb7d0b435a993f276c003366ab6a252b6ae2
KleberKeki <kleber.barbosa93@hotmail.com>
2024-06-21 18:48:14 -0300

    Desenvolvimento do exercicios de agencia de turismo

--- src/test/java/CompahiaAerea/FlashTurismo.java
+++ src/test/java/CompahiaAerea/FlashTurismo.java
@@ -7,87 +7,297 @@ public class FlashTurismo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner menu = new Scanner(System.in);
-        int opcao;
-
-        do {
-            System.out.println("Escolha Seu Destino\n");
-            System.out.println("|-----------------------------|");
-            System.out.println("| Destino 1 - Londres  - UK   |");
-            System.out.println("| Destino 2 - Munique  - GER  |");
-            System.out.println("| Destino 3 - Las Vegas- USA  |");
-            System.out.println("| Destino 4 - New York - USA  |");
-            System.out.println("| Destino 5 - Toronto  - CAN  |");
-            System.out.println("| Destino 6 - Manaus   - BR   |");
-            System.out.println("|-----------------------------|");
-
-            System.out.print("Digite o numero da opção escolhida: ");
-            opcao = menu.nextInt();
-
-            switch (opcao) {
-                case 1:
-                	int Londres;
-                    System.out.println("\nLondres  - UK  Selecionado");
-                    System.out.println("Escolha a data da viagem\n");
-                    System.out.println("|--------------------|");
-                    System.out.println("| Data 1 - 20/07/24  |");
-                    System.out.println("| Data 2 - 15/08/24  |");                                   
-                    System.out.println("| Data 3 - 23/10/24  |");
-                    System.out.println("|-----------|");
-
-                    System.out.print("Digite o numero da data escolhida: ");
+		Scanner scanner= new Scanner(System.in);
+        
+            System.out.println("Bem vindo a Compahia Aere Flash Turismo \n Aqui seu sonho vira Realidade");
+            System.out.println("Qual a data que deseja Viajar:");
+            
+            String data = scanner.next();
+            System.out.println (data +" otima sugestao de data");
+            System.out.println("atendemos os seguintes destinos: ");
+            System.out.println("|-----------|");
+            System.out.println("|  Londres  |");
+            System.out.println("|  Munique  |");
+            System.out.println("|  LasVegas |");
+            System.out.println("|  NewYork  |");
+            System.out.println("|  Toronto  |");
+            System.out.println("|  Manaus   |");
+            System.out.println("|-----------|");
+            System.out.println("Digite seu Destino ");
+            
+            String destino = scanner.next();
+            System.out.println("Otima escolha "+ destino +" é um lugar lindo");
+          
+            
+            int valorNacional = 1500;
+            int valorInternacional = 4000;                              
+                          
+                        
+            switch (destino) {
+            case "Londres":
+                	
+                    System.out.println("Voce escolheu um otimo destino os inglesss sao boas pessoas.");
+                    System.out.println("o valopr da sua passagem é 4.000 Reais.");
+                    System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                    System.out.println("Como deseja pagar avista ou parcelado?");
                     
-                    opcao = menu.nextInt();                 
-                                               
+                    String formaPagamento = scanner.next();
+                   
+                   
                     
-                    int pagamento;
-                    System.out.println("\nQual o meio de pagamento");                  
-                    System.out.println("|--------------------|");
-                    System.out.println("| Debito             |");
-                    System.out.println("| Credito a vista    |");                                   
-                    System.out.println("| Credito Parcelado  |");
-                    System.out.println("|--------------------|");
-
-                    System.out.print("Digite o meio de pagamento: ");
+                    switch (formaPagamento) {
+                    case "avista":
                     
-                    opcao = menu.nextInt();                 
-                                               
+                  
+                    	System.out.println("voce selecionou a opçao " + formaPagamento); 
+                    	System.out.println("");
+                    	System.out.println("O valor da passagem é 4.000 Reais.");
+                    	System.out.println("recebemos o pagamento " + formaPagamento + " Parabens");
+                    	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                    	System.out.println("Todos nossos voos sem do aeroporto do Galeão no Rio de Janeiro");
+                    	break; 
+                    	 
+                    	
+                    case "parcelado":                           
                     
-                    
-                    
-                    
-                    
-                    
-                    
+                    	System.out.println("voce selecionou a opçao " + formaPagamento);
+                    	System.out.println("");
+                    	System.out.println("O valor da passagem é 4.000 Reais.");
+                    	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                    	System.out.println("Digite o numero de parcelas");
+                    	int numeroparcela =scanner.nextInt();
+                    	int valordaparcela = valorInternacional/numeroparcela;
+                    	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                    	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                    	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                    	break; 
+                    	 
+                    default:
+                        System.out.println("\nOpção Inválida!");
+                        break;
+                    }
+           
+                    break;
                                  
-                                        break;
-                case 2:
-                    System.out.println("\nOpção Clientes Selecionado");
-                    break;
-                case 3:
-                    System.out.println("\nOpção Produtos Selecionado");
-                    break;
-                case 4:
-                    System.out.println("\nOpção Produtos Selecionado");
-                    break;
-                case 5:
-                    System.out.println("\nOpção Produtos Selecionado");
-                    break;
-                case 6:
-                    System.out.println("\nOpção Produtos Selecionado");
-                    break;                                 
-                           
-                case 7:
-                    System.out.println("\nAté logo!");
-                    break;
-                default:
-                    System.out.println("\nOpção Inválida!");
+              case "Munique":
+                	
+                    System.out.println("Voce escolheu um otimo destino!.");
+                    System.out.println("o valor da sua passagem é 4.000 Reais.");
+                    System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                    System.out.println("Como deseja pagar avista ou parcelado?");
+                    
+                    String formaPagamento1 =  scanner.next();
+                                     
+                    
+                    switch (formaPagamento1) {
+                    case "avista":
+                    
+                  
+                    	System.out.println("voce selecionou a opçao " + formaPagamento1); 
+                    	System.out.println("");
+                    	System.out.println("O valor da passagem é 4.000 Reais.");
+                    	System.out.println("recebemos o pagamento " + formaPagamento1 + " Parabens");
+                    	System.out.println(" Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                    	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                    	break; 
+                    	 
+                    	
+                    case "parcelado":                           
+                    
+                    	System.out.println("voce selecionou a opçao " + formaPagamento1);
+                    	System.out.println("");
+                    	System.out.println("O valor da passagem é 4.000 Reais.");
+                    	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                    	System.out.println("Digite o numero de parcelas");
+                    	int numeroparcela =scanner.nextInt();
+                    	int valordaparcela = valorInternacional/numeroparcela;
+                    	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                    	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                    	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                    	break; 
+                    	 
+                    default:
+                        System.out.println("\nOpção Inválida!");
+                        break;
+                    }
+           
+              case "LasVegas":
+              	
+                  System.out.println("Voce escolheu um otimo destino!.");
+                  System.out.println("o valopr da sua passagem é 4.000 Reais.");
+                  System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                  System.out.println("Como deseja pagar avista ou parcelado?");
+                  
+                  String formaPagamento2 =  scanner.next();
+                 
+                 
+                  
+                  switch (formaPagamento2) {
+                  case "avista":
+                  
+                
+                  	System.out.println("voce selecionou a opçao " + formaPagamento2); 
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("recebemos o pagamento " + formaPagamento2 + " Parabens");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  	
+                  case "parcelado":                           
+                  
+                  	System.out.println("voce selecionou a opçao " + formaPagamento2);
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                  	System.out.println("Digite o numero de parcelas");
+                  	int numeroparcela =scanner.nextInt();
+                  	int valordaparcela = valorInternacional/numeroparcela;
+                  	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  default:
+                      System.out.println("\nOpção Inválida!");
+                      break;
+                  }
+                  
+              case "NewYork":
+              	
+                  System.out.println("Voce escolheu um otimo destino!.");
+                  System.out.println("o valopr da sua passagem é 4.000 Reais.");
+                  System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                  System.out.println("Como deseja pagar avista ou parcelado?");
+                  
+                  String formaPagamento3 =  scanner.next();
+                  
+                 
+                  
+                  switch (formaPagamento3) {
+                  case "avista":
+                  
+                
+                  	System.out.println("voce selecionou a opçao " + formaPagamento3); 
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("recebemos o pagamento " + formaPagamento3 + " Parabens");
+                  	System.out.println(" Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  	
+                  case "parcelado":                           
+                  
+                  	System.out.println("voce selecionou a opçao" + formaPagamento3);
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                  	System.out.println("Digite o numero de parcelas");
+                  	int numeroparcela =scanner.nextInt();
+                  	int valordaparcela = valorInternacional/numeroparcela;
+                  	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                  	System.out.println("Espero que Proveite seu destino \n  por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  default:
+                      System.out.println("\nOpção Inválida!");
+                      break;
+                  }
+              case "Toronto":
+              	
+                  System.out.println("Voce escolheu um otimo destino!.");
+                  System.out.println("o valopr da sua passagem é 4.000 Reais.");
+                  System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                  System.out.println("Como deseja pagar avista ou parcelado?");
+                  
+                  String formaPagamento4 =  scanner.next();
+                 
+                  
+                  switch (formaPagamento4) {
+                  case "avista":
+                  
+                
+                  	System.out.println("voce selecionou a opçao " + formaPagamento4); 
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("recebemos o pagamento " + formaPagamento4 + " Parabens");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  	
+                  case "parcelado":                           
+                  
+                  	System.out.println("voce selecionou a opçao " + formaPagamento4);
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 4.000 Reais.");
+                  	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                  	System.out.println("Digite o numero de parcelas");
+                  	int numeroparcela =scanner.nextInt();
+                  	int valordaparcela = valorInternacional/numeroparcela;
+                  	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  default:
+                      System.out.println("\nOpção Inválida!");
+                     
+                  }
+                  
+              case "Manaus":
+              	
+                  System.out.println("Voce escolheu um otimo destino!.");
+                  System.out.println("o valopr da sua passagem é 1.500 Reais.");
+                  System.out.println("aceitamos pagamento somente no cartão de credito, a vista ou parcelado em ate 10x sem juros.");
+                  System.out.println("Como deseja pagar avista ou parcelado?");
+                  
+                  String formaPagamento5 =  scanner.next();
+                  
+                 
+                  
+                  switch (formaPagamento5) {
+                  case "avista":
+                  
+                
+                  	System.out.println("voce selecionou a opçao " + formaPagamento5); 
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 1.500 Reais.");
+                  	System.out.println("recebemos o pagamento " + formaPagamento5 + " ");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  	
+                  case "parcelado":                           
+                  
+                  	System.out.println("voce selecionou a opçao " + formaPagamento5);
+                  	System.out.println("");
+                  	System.out.println("O valor da passagem é 1.500 Reais.");
+                  	System.out.println("Realizamos o parcelamento em ate 10x sem juros");
+                  	System.out.println("Digite o numero de parcelas");
+                  	int numeroparcela =scanner.nextInt();
+                  	int valordaparcela = valorNacional/numeroparcela;
+                  	System.out.println("O valor da sua parcela fica em " + valordaparcela+ "Reais");
+                  	System.out.println("Espero que Proveite seu destino \nObrigado por voar com a Flash");                                   
+                  	System.out.println("Todos nossos voos saem do aeroporto do Galeão no Rio de Janeiro");
+                  	break; 
+                  	 
+                  default:
+                      System.out.println("\nOpção Inválida!");
+                      break;
+                  }
             }
-        } while (opcao != 7);
-
-        menu.close();
-    }
+	}
 }
+
+	                   
+                                       
+    
+	
 
 
 
